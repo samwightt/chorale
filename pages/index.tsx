280fc8a7ead845e49240d1e8178ccd2c88e6b140
Sam Wight <samuelwight@gmail.com>
2020-04-10 11:54:26 -0500

    Add static prerendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,19 @@
-const Home = () => <h1>Hello world!</h1>
+import { GetServerSideProps } from 'next'
+
+interface HomeProps {
+  random: number
+}
+
+const Home: React.FC<HomeProps> = (props) => <h1>Random number: {props.random}</h1>
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  context.res.setHeader("Cache-Control", "s-maxage=1, stale-while-revalidate")
+
+  return {
+    props: {
+      random: Math.random()
+    }
+  }
+}
 
 export default Home

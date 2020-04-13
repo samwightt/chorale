71176d43f8508917ce06ac1cfac731ae353fbf5b
Sam Wight <samuelwight@gmail.com>
2020-04-13 16:29:08 -0500

    Issues?

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,16 @@
+const purgecss = require("@fullhuman/postcss-purgecss");
+
 let plugins = ["tailwindcss", "postcss-preset-env"];
 
-const purgecss = [
-  "@fullhuman/postcss-purgecss",
-  {
-    content: ["./**/*.tsx", "./**/*.ts"],
-    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-  },
-];
+if (process.env.NODE_ENV === "production")
+  plugins.push([
+    "@fullhuman/postcss-purgecss",
+    {
+      content: ["./**/*.tsx"],
+      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    },
+  ]);
 
 module.exports = {
-  plugins: [
-    "tailwindcss",
-    "postcss-preset-env",
-    process.env.NODE_ENV === "production" ? purgecss : [],
-  ],
+  plugins: plugins,
 };

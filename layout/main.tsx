1e2c1415166e298c1ce72a1a15db927bebf2b5c8
Sam Wight <samuelwight@gmail.com>
2020-04-14 02:08:32 -0500

    Add Google Analytics

--- layout/main.tsx
+++ layout/main.tsx
@@ -2,7 +2,25 @@ import React from "react";
 import Head from "next/head";
 
 const Layout: React.FC = (props) => {
-  return <>{props.children}</>;
+  return (
+    <>
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-163568155-1"
+        ></script>
+        <script>
+          {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-163568155-1');`}
+        </script>
+      </Head>
+      {props.children}
+    </>
+  );
 };
 
 export default Layout;

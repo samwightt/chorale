9b32024640ccbe9cc987c61a5231b47276c2fdba
Sam Wight <samuelwight@gmail.com>
2020-06-19 10:56:15 -0500

    Start adding typed_html.

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,16 +1,21 @@
 use crate::parser::*;
-use horrorshow::prelude::*;
-use horrorshow::helper::doctype;
 use anyhow::{anyhow, Result};
+use typed_html::{html, dom::DOMTree};
 
-// pub fn render(data: LoadPageChunkData, id: String) -> Result<()> {
-//     let root = data.record_map.block.get(&id).ok_or(anyhow!("Could not find root block."))?;
-//     if let RootBlockType::Page { 
-//         data, 
-//         format, 
-//         file_ids, 
-//         permissions } = &root.value {
-//        println!("It's a page!");
-//     }
-//     Ok(())
-// }
+pub fn render(data: LoadPageChunkData, id: String) -> Result<()> {
+    let root = data.record_map.block.get(&id).ok_or(anyhow!("Could not find root block."))?;
+    if let Either::Left(RootBlockType::Page { 
+        data, 
+        format, 
+        file_ids
+        }) = &root.value {
+        let hello: DOMTree<String> = html!(
+            <h1>"This works!"</h1>
+        );
+        println!("{}", hello.to_string());
+        Ok(())
+    }
+    else {
+        Err(anyhow!("Root type must be a page."))
+    }
+}
